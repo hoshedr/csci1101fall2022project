5afc274fcda6ac6b9901922a4f89be5a36df9c08
Joshua Rodriguez <Joshua.j.rodriguez01@utrgv.edu>
2022-12-06 18:32:52 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int16_value, string_value, datetime_value) VALUES (2045, 'goodbye world', '2017-08-01 00:00:01');
+
+--UPDATE sandbox SET int32_value = -1024620 WHERE id = 112;
+
+--DELETE FROM sandbox WHERE id = 85;
+
+SELECT * FROM sandbox;
